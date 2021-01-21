c437bd17410f8238aae2ffaf6044c3597bacb775
Nikita Stets <Nikita.stets.999@mail.ru>
2021-01-21 14:58:43 +0300

    Add controller without ApiController

---
+++ ASP.NET Core Web APIs/Controllers/CarsController.cs
@@ -0,0 +1,20 @@
+﻿using ASP.NET_Core_Web_APIs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Web_APIs.Controllers
+{
+    [Route("{controller}/{action}/{id}")]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public Car GetById(int id)
+        {
+            return new Car
+            {
+                Id = id,
+                MakeName = "Chevrolet",
+                ModelName = "Camaro"
+            };
+        }
+    }
+}
---
+++ ASP.NET Core Web APIs/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_Web_APIs.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string MakeName { get; set; }
+
+        public string ModelName { get; set; }
+    }
+}
