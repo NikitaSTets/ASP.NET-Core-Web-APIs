c1158e6bfeefe07697c769effb2530a711bb4151
Nikita Stets <Nikita.stets.999@mail.ru>
2021-01-25 14:39:41 +0300

    Rest API example

--- ASP.NET Core Web APIs/Controllers/CarsController.cs
+++ ASP.NET Core Web APIs/Controllers/CarsController.cs
@@ -1,12 +1,33 @@
-﻿using ASP.NET_Core_Web_APIs.Models;
+﻿using System.Collections.Generic;
+using ASP.NET_Core_Web_APIs.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_Core_Web_APIs.Controllers
 {
-    [Route("{controller}/{action}/{id}")]
+    [Route("{controller}")]
     public class CarsController : ControllerBase
     {
         [HttpGet]
+        public ActionResult<IEnumerable<Car>> CarsList()
+        {
+            return new List<Car>
+            {
+                new Car
+                {
+                    Id = 15,
+                    MakeName = "Chevrolet",
+                    ModelName = "Camaro"
+                },
+                new Car
+                {
+                    Id = 123,
+                    MakeName = "Audi",
+                    ModelName = "A6"
+                }
+        };
+        }
+
+        [HttpGet("{id}")]
         public Car GetById(int id)
         {
             return new Car
@@ -15,6 +36,40 @@ namespace ASP.NET_Core_Web_APIs.Controllers
                 MakeName = "Chevrolet",
                 ModelName = "Camaro"
             };
+        }
+
+        [HttpGet("{carId}/{action}")]
+        public ActionResult<List<People>> Owners(int carId)
+        {
+            return new List<People>
+            {
+                new People
+                {
+                    Name = "name",
+                    SurName = "surName",
+                    CarId = carId
+                },
+                new People
+                {
+                    Name = "newName",
+                    SurName = "surName",
+                    CarId = carId
+                }
+            };
+        }
+
+        [HttpDelete("{carId}")]
+        public ActionResult DeleteCar(int carId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("create")]
+        public ActionResult CreateCar([FromBody]Car car)
+        {
+            car.Id = 15;
+
+            return CreatedAtAction(nameof(GetById), new { id = car.Id }, car);
         }
     }
 }
---
+++ ASP.NET Core Web APIs/Models/People.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_Web_APIs.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public int CarId { get; set; }
+    }
+}
